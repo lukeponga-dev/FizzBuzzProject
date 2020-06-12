4be724c41b645abee7233adf85937f28ca5545d7
lukeg <lukeg@DESKTOP-NORVVCH>
2020-06-13 03:02:56 +1200

    Add project files.

---
+++ FizzBuzzProject/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace FizzBuzzProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListOutput = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListOutput
+            // 
+            this.ListOutput.FormattingEnabled = true;
+            this.ListOutput.Location = new System.Drawing.Point(389, 22);
+            this.ListOutput.Name = "ListOutput";
+            this.ListOutput.Size = new System.Drawing.Size(302, 264);
+            this.ListOutput.TabIndex = 0;
+            this.ListOutput.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 89);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "FuzzBuzz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(774, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ListOutput);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox ListOutput;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FizzBuzzProject/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FizzBuzzProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (int i= 1 ; i < 100; i++)
+            {
+                //  dividable by 15 - FizzBuzz
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    ListOutput.Items.Add("FizzBuzz");
+                }
+                // dividable by 5 - Buzz
+                else if (i % 5 == 0)
+                {
+                    ListOutput.Items.Add("Buzz");
+                }
+                // dividable by 3 - Fizz
+                else if (i % 3 == 0)
+                {
+                    ListOutput.Items.Add("Fizz");
+                }
+                //  List Numbers itself
+                else
+                {
+                    ListOutput.Items.Add(i);
+                }
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    } 
+}
